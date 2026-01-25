94147c27626e0dc55f6025c4bcfeef86a08a786b
davidtole-ss <davidtole62@gmail.com>
2026-01-25 15:36:10 +0100

    Ejemplos arrays.

--- Java/T6/EjerciciosClaseString/src/Ejercicio03/Main.java
+++ Java/T6/EjerciciosClaseString/src/Ejercicio03/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner datos = new Scanner(System.in);
-        System.out.println("Introduce una palabra.");
+        System.out.println("Introduce una palabra..");
         String palabra = datos.nextLine();
         int cuentaVocales = 0;
         for (int i = 0; i < palabra.length(); i++) {
