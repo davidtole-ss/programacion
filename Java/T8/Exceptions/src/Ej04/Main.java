fa2f076f55d435c9c008bccef4238fc9b846a690
unknown <davidtole62@.com>
2026-02-17 15:40:17 +0100

    Ejercicios excepciones

--- Java/T8/Exceptions/src/Ej04/Main.java
+++ Java/T8/Exceptions/src/Ej04/Main.java
@@ -1,7 +1,24 @@
 package Ej04;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner datos= new Scanner(System.in);
+        boolean seguir= true;
+        int numero=0;
+        while(seguir){
+            try{
+                System.out.println("Introduce números, 0 para salir: ");
+                 numero=datos.nextInt();
+                if(numero==0){
+                    seguir=false;
+                }
+            } catch (Exception e) {
+                System.out.println("¡Cuidado! Solo puedes escribir números enteros.");
+                datos.nextLine(); // Limpia lo que el usuario escribió mal
+            }
 
+        }
     }
 }
---
+++ Java/T8/Exceptions/src/Ej05/Main.java
@@ -0,0 +1,5 @@
+package Ej05;
+
+public class Main {
+
+}
