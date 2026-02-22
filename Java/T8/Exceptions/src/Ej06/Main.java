632f2934c11135a5d25c46752e82e8c03f3d32cd
unknown <davidtole62@.com>
2026-02-22 22:25:00 +0100

    Actualizar Ej06 y eliminar Ej07

--- Java/T8/Exceptions/src/Ej06/Main.java
+++ Java/T8/Exceptions/src/Ej06/Main.java
@@ -4,23 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner datos= new Scanner(System.in);
-        System.out.println("Introduce una temperatura en grados celsius.");
-        double gradosCelsius= datos.nextInt();
-        convertirTemperatura(gradosCelsius);
-
     }
-    public static void convertirTemperatura(double temperatura){
-        try{
-            if(temperatura<=-273.15){
-                throw new Exception("Los grados celsius no pueden estar por debajo de -273.15.");
-            }
-            double fh=(temperatura * 9 / 5) + 32;
-            System.out.printf("%.2f grados celsius son %.2f grados Fahrenheit.",temperatura,fh);
-        }catch(Exception e){
-            System.out.println(e.getMessage());
 
-
-        }
-    }
 }
